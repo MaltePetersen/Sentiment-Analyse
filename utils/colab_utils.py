ee70ce0f2625f11d302da29adf27a4b6858f2cd0
Malte Petersen <mp@marketoolz.com>
2023-07-09 05:11:08 +0200

    Colab entfernt und Fazit transformer verfeinert

--- utils/colab_utils.py
+++
@@ -1,8 +0,0 @@
-from google.colab import files
-
-def upload_kaggle_file():
-    uploaded = files.upload()
-
-    for fn in uploaded.keys():
-        print('User uploaded file "{name}" with length {length} bytes'.format(
-            name=fn, length=len(uploaded[fn])))
