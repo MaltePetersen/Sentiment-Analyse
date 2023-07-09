65c6131710d317ccd7899a33164d1aa8a79874c4
p-hlp <lamp.phillip@gmail.com>
2023-07-09 20:44:07 +0200

    Revisite text utils

--- utils/text_utils.py
+++ utils/text_utils.py
@@ -1,16 +1,35 @@
 import re
 from nltk.tokenize import word_tokenize
-
 from nltk.corpus import stopwords
+import contractions
+
 stopwords = set(stopwords.words('english'))
 
-def clean_text(text):
+def clean_text(text, disable_stopwords=False):
     # Convert to lowercase
-    test = text.lower()
-    # Remove punctuation
-    test = re.sub(r'[^a-z\s]', '', test)
+    tmp = text.lower()
+    
+    # Expand contractions return string
+    tmp = str(contractions.fix(tmp))
+
     # Remove HTML Tags
-    test = re.sub(r'<.*?>', '', test)
+    # tmp = BeautifulSoup(str(tmp), 'html.parser').get_text()
+
+    # Remove non-ASCII characters
+    tmp = tmp.encode('ascii', 'ignore').decode('ascii')
+
+    # Remove HTML Tags with regex
+    tmp = re.sub(r'<.*?>', '', tmp)
+
+    # Remove URLs
+    tmp = re.sub(r'http\S+', '', tmp)
+
+    # Remove punctuation
+    tmp = re.sub(r'[^\w\s]', '', tmp)
+
+
     # Remove stopwords
-    test = ' '.join([word for word in word_tokenize(test) if word not in stopwords])
-    return test
+    # if not disable_stopwords:
+        # tmp = ' '.join([word for word in word_tokenize(tmp) if word not in stopwords])
+
+    return tmp 
