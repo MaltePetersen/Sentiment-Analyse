797a84546cacf3b84ba79478e59e5de597fd88bc
p-hlp <lamp.phillip@gmail.com>
2023-07-10 20:56:24 +0200

    Clean up EDA Notebook

--- utils/plot_utils.py
+++ utils/plot_utils.py
@@ -4,6 +4,14 @@ import seaborn as sns
 
 
 def plot_history_metrics(history, metrics):
+    """
+    Plot the history of the metrics of the model
+
+    Args:
+        history: keras history object.
+        metrics: list of strings, the metrics to plot.
+    """
+
     for metric in metrics:
         plt.plot(history.history[metric])
         plt.plot(history.history['val_'+metric])
@@ -14,6 +22,16 @@ def plot_history_metrics(history, metrics):
 
 
 def get_classification_report(model, X_test, y_test, y_pred_bound=0.85):     
+    """
+    Plot the ROC curve and the confusion matrix of the model
+
+    Args:
+        model: keras model object.
+        X_test: numpy array, the test data.
+        y_test: numpy array, the test labels.
+        y_pred_bound: float, the threshold for the predicted probabilities.
+    """
+
     y_pred = model.predict(X_test)
     fpr, tpr, _ = roc_curve(y_test,  y_pred)
     auc = roc_auc_score(y_test, y_pred)
@@ -29,3 +47,53 @@ def get_classification_report(model, X_test, y_test, y_pred_bound=0.85):
     plt.figure(figsize=(10, 8))
     sns.heatmap(confusion_matrix(y_test, y_pred),
                 annot=True, fmt='.4g', cmap='Blues')
+
+
+def plot_ngrams_counts(ngrams, plot_title):
+    """
+    Function to plot the most common n-grams.
+    
+    Args:
+        ngrams: List of tuples, the most common n-grams and their counts. E.g. [(('in', 'the'), 100), (('of', 'the'), 90), ...] for Bigrams.
+        plot_title: String, the title of the plot.
+    """
+
+    # Separate the n-grams and their counts for plotting
+    ngrams_list = [ngram[0] for ngram in ngrams]
+    counts = [ngram[1] for ngram in ngrams]
+
+    # Format the n-grams for plotting, the ngrams are in format
+    ngrams_list = [' '.join(ngram) for ngram in ngrams_list]
+
+    # Get the n of the n-grams
+    n = len(ngrams_list[0].split())
+
+    # Create the plot
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x=counts, y=ngrams_list)
+    plt.xlabel('Count')
+    plt.ylabel(f'{n}-gram')
+    plt.title(plot_title)
+    plt.show()
+
+
+def plot_words_counts(words, plot_title):
+    """
+    Function to plot the most common words.
+    
+    Args:
+        words: List of tuples, the most common words and their counts. E.g. [('the', 100), ('of', 90), ...]
+        plot_title: String, the title of the plot.
+    """
+
+    # Separate the words and their counts for plotting
+    words_list = [word[0] for word in words]
+    counts = [word[1] for word in words]
+
+    # Create the plot
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x=counts, y=words_list)
+    plt.xlabel('Count')
+    plt.ylabel('Word')
+    plt.title(plot_title)
+    plt.show()
--- utils/text_utils.py
+++ utils/text_utils.py
@@ -1,19 +1,28 @@
 import re
-from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import contractions
+from collections import Counter
+import nltk
 
 stopwords = set(stopwords.words('english'))
 
 def clean_text(text, disable_stopwords=False):
+    """
+    Clean text by removing unnecessary characters and altering the format of words.
+
+    Args:
+        text (str): The string of text to be cleaned.
+        disable_stopwords (bool): Whether to remove stopwords or not
+    
+    Returns:
+        str: The cleaned text.
+    """
+
     # Convert to lowercase
     tmp = text.lower()
     
     # Expand contractions return string
     tmp = str(contractions.fix(tmp))
-
-    # Remove HTML Tags
-    # tmp = BeautifulSoup(str(tmp), 'html.parser').get_text()
 
     # Remove non-ASCII characters
     tmp = tmp.encode('ascii', 'ignore').decode('ascii')
@@ -33,3 +42,55 @@ def clean_text(text, disable_stopwords=False):
         # tmp = ' '.join([word for word in word_tokenize(tmp) if word not in stopwords])
 
     return tmp 
+
+
+def get_most_common_words(df, column, num_words=10):
+    """
+    Function to get the most common words in a given column of a DataFrame.
+    
+    Args:
+    df: pandas DataFrame.
+    column: str, column in df which should be analyzed.
+    num_words: int, the number of most common words to return.
+    
+    Returns:
+    most_common_words: List of tuples, the most common words and their counts.
+    """
+
+    # Split the text into words
+    words = df[column].str.split(expand=True).stack()
+
+    # Count the frequency of each word
+    word_counts = Counter(words)
+
+    # Get the most common words
+    most_common_words = word_counts.most_common(num_words)
+    
+    return most_common_words
+
+
+def get_most_common_ngrams(df, column, n=2, num_ngrams=10):
+    """
+    Function to get the most common n-grams in a given column of a DataFrame.
+    
+    Args:
+    df: pandas DataFrame.
+    column: str, column in df which should be analyzed.
+    n: int, the size of the n-grams.
+    num_ngrams: int, the number of most common n-grams to return.
+    
+    Returns:
+    most_common_ngrams: List of tuples, the most common n-grams and their counts.
+    """
+
+    # Extract n-grams from the given column of the DataFrame, do this in memory, don't write to df
+    ngrams_list = df[column].apply(lambda x: list(nltk.ngrams(x.split(), n))).tolist()
+
+    # Flatten the list of n-grams and count the frequency of each n-gram
+    ngrams = [ngram for sublist in ngrams_list for ngram in sublist]
+    ngram_counts = Counter(ngrams)
+
+    # Get the most common n-grams
+    most_common_ngrams = ngram_counts.most_common(num_ngrams)
+    
+    return most_common_ngrams
